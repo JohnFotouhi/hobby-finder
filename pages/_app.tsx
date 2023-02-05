b20e84acf686e960beba8c99f76fd714e070068f
John Fotouhi <fotouman@gmail.com>
2023-02-04 19:12:10 -0600

    add next-firebase-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout";
+import initAuth from '../initAuth';
 
+initAuth();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
