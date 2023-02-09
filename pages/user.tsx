13ac5954c063e0b101cad24930ba13970ded61aa
zclevin <zlevin@wustl.edu>
2023-02-09 16:36:34 -0600

    don't know why vercel is mad

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,19 +1,19 @@
-import { Button, Col, Container, Row, Form, Stack, Alert, Navbar } from "react-bootstrap";
+// import { Button, Col, Container, Row, Form, Stack, Alert, Navbar } from "react-bootstrap";
 
-export default function User() {
+// export default function User() {
     
-    return(
-        <>  
-            <Container fluid className ="bg-light">
-                {/* Name */}
-                <h1>Jane Doe</h1> 
-                {/* Username */}
-                <p>JanePlaysBass</p>
-                {/* Bio */}
-                <p>I'm Jane and I play the basss. Yipee!</p>
+//     return(
+//         <>  
+//             <Container fluid className ="bg-light">
+//                 {/* Name */}
+//                 <h1>Jane Doe</h1> 
+//                 {/* Username */}
+//                 <p>JanePlaysBass</p>
+//                 {/* Bio */}
+//                 <p>I'm Jane and I play the basss. Yipee!</p>
 
-            </Container>
-        </>
-    );
+//             </Container>
+//         </>
+//     );
 
-}
+// }
