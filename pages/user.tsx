33808a20d07412fa60a50cc9e0dcd516a0262477
zclevin <zlevin@wustl.edu>
2023-02-10 15:44:17 -0600

    GRRRRR vercel

--- pages/user.tsx
+++ pages/user.tsx
@@ -9,7 +9,7 @@ export default function User() {
 
                 {/* TODO: For each hobby card in the database associated with the user, populate a hobby card component */}
                 <HobbyCard instrument={"Bass"} genre={"rock"} experience={"2 years - Beginner"} commitment={"Looking to join a band"} 
-                info={"Id really love to join a band, but I don't care if we're trash!"} clip={null} owner={true}></HobbyCard>
+                info={"Id really love to join a band, but I dont care if we are trash!"} clip={null} owner={true}></HobbyCard>
 
             </Container>
         </>
