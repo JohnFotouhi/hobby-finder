b05023e03d2547d01f41f2a24a2e8af3044f0e24
NMaguigad <n.p.maguigad@wustl.edu>
2023-02-14 17:44:56 -0600

    working on upload image component

---
+++ components/uploadImage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Form } from "react-bootstrap"
+
+
+
+export default function FormInput({controlId, label, type, placeholder, text}){
+ return(
+    <Form.Group className="mb-3" controlId={controlId}>
+        <Form.Label>{label}</Form.Label>
+        <Form.Control type={type} placeholder={placeholder} />
+        <Form.Text className="text-muted">{text}</Form.Text>
+    </Form.Group>
+ );
+}
+
