5cee4b13e3503f36920165a023f1b59a4602f365
John Fotouhi <fotouman@gmail.com>
2023-02-20 19:10:06 -0600

    add example users on search page

--- components/formInput.tsx
+++ components/formInput.tsx
@@ -1,11 +1,11 @@
 import { Form } from "react-bootstrap"
 
-export default function FormInput({controlId, label, type, placeholder, text, setValue, value}){
+export default function FormInput({controlId, label, type, placeholder, text, setValue, value, ...props}){
 
  return(
     <Form.Group className="mb-3" controlId={controlId}>
         <Form.Label>{label}</Form.Label>
-        <Form.Control type={type} placeholder={placeholder} onChange={e => setValue(e.target.value)} value={value}/>
+        <Form.Control type={type} placeholder={placeholder} onChange={e => setValue(e.target.value)} value={value} min={(type === "number") ? props.min : undefined}/>
         <Form.Text className="text-muted">{text}</Form.Text>
     </Form.Group>
  );
