f4d76314d21fb7136a0b35d3934095d238bd407b
John Fotouhi <fotouman@gmail.com>
2023-02-25 18:20:48 -0600

    add email verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,26 @@
+import React from 'react';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout";
 import initAuth from '../initAuth';
+import { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+
 
 initAuth();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [verifyEmail, setVerifyEmail] = useState(false);
   return (
     <Layout>
-      <Component {...pageProps} />
+      <Component {...pageProps} setVerifyEmail={setVerifyEmail} />
+      <Modal show={verifyEmail}>
+        <Modal.Header >
+          <Modal.Title>Please check your email and follow the verification link to verify your account.</Modal.Title>
+          {/* TODO: Add link to resend verification email and prevent refresh workaround*/}
+        </Modal.Header>
+      </Modal>
     </Layout>
   );
 }
