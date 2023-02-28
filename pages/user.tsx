7a287e39f1779d34eb540c85ccb254ef8021ce4c
zclevin <zlevin@wustl.edu>
2023-02-28 17:28:02 -0600

    trying to get past vercel

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,9 +1,9 @@
 import { Button, Col, Container, Row, Form, Stack, Alert, Navbar } from "react-bootstrap";
-import HobbyCard from "@/components/hobbyCard";
+import HobbyCard from "../components/hobbyCard";
 import { useAuthUser, AuthAction, withAuthUser } from "next-firebase-auth";
-import UploadImage from "@/components/uploadImage";
-import UserInformation from "@/components/userInformation";
-import perry from "@/public/User_images/perry.png";
+import UploadImage from "../components/uploadImage";
+import UserInformation from "../components/userInformation";
+//import perry from "../public/User_images/perry.png";
 
 function User() {
 
@@ -18,14 +18,14 @@ function User() {
 
     return(
         <>  
-            <UserInformation capacity={"2"} equipment={"a condenser mic and an interface"} schedule = {"Any morning before 11am"} displayName={"Perry the Platypus"} bio={"*chatter*"} owner={false} editProfile={editProfile} profilePicture={perry}></UserInformation>
+            {/*<UserInformation capacity={"2"} equipment={"a condenser mic and an interface"} schedule = {"Any morning before 11am"} displayName={"Perry the Platypus"} bio={"*chatter*"} owner={false} editProfile={editProfile} profilePicture={perry}></UserInformation>*/}
             <Container fluid className ="bg-light">
 
                 <Col><Button>Reach Out</Button></Col>
 
                 {/* TODO: For each hobby card in the database associated with the user, populate a hobby card component */}
                 <HobbyCard uid={AuthUser.id} setCards={null} index={0} instrument={"Bass"} genre={"rock"} experience={"2 years - Beginner"} commitment={"Looking to join a band"} 
-                info={"Id really love to join a band, but I dont care if we are trash!"} owner={false} editCard={null}></HobbyCard>
+                info={"Id really love to join a band, but I dont care if we are trash!"} owner={false} editCard={undefined}></HobbyCard>
 
             </Container>
         </>
