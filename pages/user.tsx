4d0011ebddad06b82093c4a2e65b6dddf2370db1
zclevin <zlevin@wustl.edu>
2023-02-28 17:32:10 -0600

    pls vercel

--- pages/user.tsx
+++ pages/user.tsx
@@ -23,9 +23,9 @@ function User() {
 
                 <Col><Button>Reach Out</Button></Col>
 
-                {/* TODO: For each hobby card in the database associated with the user, populate a hobby card component */}
-                <HobbyCard uid={AuthUser.id} setCards={null} index={0} instrument={"Bass"} genre={"rock"} experience={"2 years - Beginner"} commitment={"Looking to join a band"} 
-                info={"Id really love to join a band, but I dont care if we are trash!"} owner={false} editCard={undefined}></HobbyCard>
+                {/* TODO: For each hobby card in the database associated with the user, populate a hobby card component 
+                <HobbyCard uid={AuthUser.id} setCards={null} index={0} instrument={"Bass"} genre={"rock"} experience={"2 years - Beginner"} commitMin={"1"} 
+                info={"Id really love to join a band, but I dont care if we are trash!"} owner={false} editCard={undefined}></HobbyCard> */}s
 
             </Container>
         </>
